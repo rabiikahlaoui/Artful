18549cc5c2c72bbb8516ee397a8633fa1094a857
Rabii Kahlaoui <rabii.kahlaouiii@gmail.com>
2022-06-16 22:57:42 +0100

    Install dependencies and add ThirdwebProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ThirdwebProvider
+      desiredChainId={ChainId.Goerli}
+      chainRpc={{
+        [ChainId.Goerli]: 'https://goerli.infura.io/v3/cdeaf49fb71d454894b0c3a143cc26bf'
+      }}
+    >
+        <Component {...pageProps} />
+    </ThirdwebProvider>
+  )
 }
 
 export default MyApp
